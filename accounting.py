ec0f0e08aa8a175a5b908757f8309f44ba22b980
Elizabeth Chin <elizabethtchin@gmail.com>
2019-04-28 08:27:27 +0000

    commented code

--- accounting.py
+++ accounting.py
@@ -5,18 +5,18 @@ def accounting(file):
 
     the_file = open(file) #opens file
     for line in the_file:
-        line = line.rstrip()
-        words = line.split("|")
+        line = line.rstrip()#removes white space
+        words = line.split("|")#creates list of strings
 
-        customer_name = words[1]
-        melons_purchased = float(words[2])
-        amt_paid = float(words[3])
+        customer_name = words[1] #gets customers name
+        melons_purchased = float(words[2]) #gets melons purchased, change to float
+        amt_paid = float(words[3])#gets amount paid, change to float
 
 
-    the_file.close()
+    the_file.close() #close file bc finished
 
-    customer_expected = melons_purchased * melon_cost
-    if customer_expected != amt_paid:
+    customer_expected = melons_purchased * melon_cost#cal what customer owes
+    if customer_expected != amt_paid:#if customer didn't pay what was owed
         print(f"{customer_name} paid ${amt_paid:.2f}, expected", 
         f"${customer_expected:.2f}")
     
